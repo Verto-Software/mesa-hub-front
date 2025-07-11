48625f756d029ea96cab7a46437d854c2d86a43e
Jack <ferreira.marlon@live.com>
2025-07-11 15:13:14 -0300

    feat: melhora estrutura da home page

--- src/app/[locale]/(home)/page.tsx
+++ src/app/[locale]/(home)/page.tsx
@@ -1,11 +1,5 @@
-import { useTranslations } from 'next-intl';
+import { Home } from '@/modules/home/home';
 
 export default function HomePage() {
-   const t = useTranslations();
-
-   return (
-      <div className='min-h-screen flex items-center justify-center bg-gradient-to-br from-purple-50 via-white to-purple-100 p-4'>
-         <h1 className='text-2xl'>{t('Title')}</h1>
-      </div>
-   );
+   return <Home />;
 }
