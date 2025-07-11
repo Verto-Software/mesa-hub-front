08589030648d936903cc0d9f322cb197c92b9851
Jack <ferreira.marlon@live.com>
2025-07-11 10:45:00 -0300

    refactor: tela home

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,9 @@
 import { Button } from '@/_ui/button';
 
 export default function HomePage() {
-   return <Button>Olá mundo - MesaHub</Button>;
+   return (
+      <div className='min-h-screen flex items-center justify-center bg-gradient-to-br from-purple-50 via-white to-purple-100 p-4'>
+         <h1 className='text-2xl'>Bem vindo!</h1>
+      </div>
+   );
 }
