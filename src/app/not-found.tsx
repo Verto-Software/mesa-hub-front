18e6bb2c9333b1c449ff0b213f0f3ba61d48bbf4
Jack <ferreira.marlon@live.com>
2025-07-11 16:19:25 -0300

    feat: finaliza a tela da página notfound 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,26 @@
+import { Button } from '@/_ui/button';
+import { Routes } from '@/routes/routes';
+import Image from 'next/image';
+import Link from 'next/link';
+
 export default function NotFoundPage() {
    return (
-      <div>
-         <h1>Not found</h1>
-      </div>
+      <section className='min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-purple-300 via-white to-purple-400 p-4'>
+         <Image
+            src='/not-found.svg'
+            alt='Página não encontrada'
+            width={400}
+            height={400}
+         />
+         <div className='flex flex-col items-center justify-center gap-6 mt-10'>
+            <p className='text-gray-800 font-bold text-xl'>404 — Até nós estamos procurando essa página!</p>
+            <Button
+               animated
+               asChild
+            >
+               <Link href={Routes.Home}>Tela de início</Link>
+            </Button>
+         </div>
+      </section>
    );
 }
