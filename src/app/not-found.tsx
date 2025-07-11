67eb7a2dfc7b3afdd369d821e12d67d521d2b87e
Jack <ferreira.marlon@live.com>
2025-07-11 16:40:37 -0300

    refactor: diminui tamanho da foto da página notfound

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,15 +5,15 @@ import Link from 'next/link';
 
 export default function NotFoundPage() {
    return (
-      <section className='min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-purple-300 via-white to-purple-400 p-4'>
+      <section className='min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-purple-300 via-white to-purple-300 p-4'>
          <Image
             src='/not-found.svg'
             alt='Página não encontrada'
-            width={400}
-            height={400}
+            width={300}
+            height={300}
          />
          <div className='flex flex-col items-center justify-center gap-6 mt-10'>
-            <p className='text-gray-800 font-bold text-xl'>404 — Até nós estamos procurando essa página!</p>
+            <p className='text-gray-800 font-bold text-xl text-center'>404 — Até nós estamos procurando essa página!</p>
             <Button
                animated
                asChild
