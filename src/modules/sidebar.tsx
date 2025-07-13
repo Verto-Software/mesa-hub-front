05695ce577f2ca0c2f44b2e11e4c5770a59ef606
Jack <ferreira.marlon@live.com>
2025-07-13 15:06:25 -0300

    style: melhoria de estilização

--- src/modules/sidebar.tsx
+++ src/modules/sidebar.tsx
@@ -153,15 +153,15 @@ export function SidebarTemplate() {
                </SidebarMenu>
             </SidebarGroup>
 
-            <div className='flex gap-3 items-center justify-center border border-gray-200 rounded-md p-2'>
-               <Avatar className='rounded-md w-10 h-10'>
+            <div className='flex gap-3 items-center justify-center border border-gray-200 rounded-md py-2 hover:bg-gray-100'>
+               <Avatar className='rounded-md  w-12 h-12'>
                   <AvatarImage
-                     className='rounded-md w-10 h-10'
+                     className='rounded-md w-12 h-12'
                      src='https://github.com/shadcn.png'
                   />
                   <AvatarFallback>CN</AvatarFallback>
                </Avatar>
-               <div className='text-sm text-gray-800'>
+               <div className='text-xs text-gray-800 '>
                   <p>ferreira.marlon@live.com</p>
                   <p>Principal</p>
                </div>
