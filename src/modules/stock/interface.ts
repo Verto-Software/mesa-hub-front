b7e68c4f73dc1b70f3d7b8ecf8fe80ef7dfeef98
Jack <ferreira.marlon@live.com>
2025-07-15 00:30:55 -0300

    feat: organize interface

--- src/modules/stock/interface.ts
+++ src/modules/stock/interface.ts
@@ -5,17 +5,16 @@ export interface TInputField {
    register: UseFormRegister<FormStockSchema>;
    errors: FieldErrors<FormStockSchema>;
 }
-
-export interface TInputQuantity extends TInputField {}
-export interface TInputName extends TInputField {}
-export interface TInputPrice extends TInputField {}
-export interface TInputSupplier extends TInputField {}
 export interface TStockActions {
    isValid: boolean;
 }
 export interface TInputTextarea extends TInputField {
    watch: UseFormWatch<FormStockSchema>;
 }
+export interface TInputQuantity extends TInputField {}
+export interface TInputName extends TInputField {}
+export interface TInputPrice extends TInputField {}
+export interface TInputSupplier extends TInputField {}
 export interface TInputMaximumStock extends TInputField {}
 export interface TInputMinimumStock extends TInputField {}
 export interface TInputCurrentStock extends TInputField {}
