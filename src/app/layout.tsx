d595ca33231917caf9f12cfa56667042d2381b28
Jack <ferreira.marlon@live.com>
2025-07-18 17:55:25 -0300

    feat: add font poppins

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,16 +2,33 @@ import { Metadata } from 'next';
 import { ReactNode } from 'react';
 import './globals.css';
 import { NextIntlClientProvider } from 'next-intl';
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({
+   subsets: ['latin'],
+   weight: ['400', '500', '600', '700'],
+   variable: '--font-poppins',
+   display: 'swap',
+});
 
 export const metadata: Metadata = {
    title: 'Not found 404',
    description: 'Sistema de gestão para restaurantes.',
 };
 
-export default async function LocaleLayout({ children, params }: { children: ReactNode; params: Promise<{ locale: string }> }) {
+export default async function LocaleLayout({
+   children,
+   params,
+}: {
+   children: ReactNode;
+   params: Promise<{ locale: string }>;
+}) {
    const { locale } = await params;
    return (
-      <html lang={locale}>
+      <html
+         lang={locale}
+         className={poppins.className}
+      >
          <body className='h-screen'>
             <NextIntlClientProvider locale={locale}>{children}</NextIntlClientProvider>
          </body>
