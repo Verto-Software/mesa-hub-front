d1bac876ef067881937e87014a005e53b78e5a14
Jack <ferreira.marlon@live.com>
2025-07-19 01:10:32 -0300

    style: card profile sidebar

--- src/_shared/components/sidebar/components/sidebar-card-profile.tsx
+++ src/_shared/components/sidebar/components/sidebar-card-profile.tsx
@@ -1,19 +1,22 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/_shared/components/ui/avatar';
+import { Card, CardContent } from '../../ui/card';
 
 export function SidebarCardProfile() {
    return (
-      <div className='flex gap-3 items-center justify-center border border-gray-200 rounded-md py-2 hover:bg-gray-100'>
-         <Avatar className='rounded-md  w-12 h-12'>
-            <AvatarImage
-               className='rounded-md w-12 h-12'
-               src='https://github.com/shadcn.png'
-            />
-            <AvatarFallback>CN</AvatarFallback>
-         </Avatar>
-         <div className='text-sm text-gray-500 font-medium'>
-            <p>ferreira.marlon@live.com</p>
-            <p>Principal</p>
-         </div>
-      </div>
+      <Card className='!p-2'>
+         <CardContent className='flex gap-2 items-center justify-center'>
+            <Avatar className='rounded-md  w-12 h-12'>
+               <AvatarImage
+                  className='rounded-md w-12 h-12'
+                  src='https://github.com/shadcn.png'
+               />
+               <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <div className='text-xs text-gray-500 font-medium'>
+               <p>ferreira.marlon@live.com</p>
+               <p>Principal</p>
+            </div>
+         </CardContent>
+      </Card>
    );
 }
