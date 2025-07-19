254281f3ddde02cb7624fcf74992f739fa9feb77
Jack <ferreira.marlon@live.com>
2025-07-19 12:05:23 -0300

    feat: adiciona uma condição caso não tenha item no estoque mostre o texto "Você não possui item no estoque."

--- src/modules/stock/table/table-items.tsx
+++ src/modules/stock/table/table-items.tsx
@@ -33,7 +33,6 @@ export function TableItems() {
          get status() {
             const current = parseInt(this.currentstock);
             const min = parseInt(this.minimumstock);
-
             if (current <= min) return 'Baixo';
             return 'Normal';
          },
@@ -42,81 +41,88 @@ export function TableItems() {
 
    return (
       <div className='px-6 pb-6'>
-         <div className='border border-gray-200 rounded-md'>
-            <Table>
-               <TableHeader>
-                  <TableRow>
-                     {itemsHead.map((itemHead) => (
-                        <TableHead
-                           key={itemHead}
-                           className='text-gray-500 pl-4'
-                        >
-                           {itemHead}
-                        </TableHead>
-                     ))}
-                  </TableRow>
-               </TableHeader>
-               <TableBody>
-                  {itemsMenu.map((itemMenu) => (
-                     <TableRow
-                        key={itemMenu.id}
-                        className='hover:bg-muted/50 border-b border-gray-200'
-                     >
-                        <TableCell className='px-4 py-4'>{itemMenu.itemname}</TableCell>
-                        <TableCell className='px-4 py-4'>{itemMenu.itemquantity}</TableCell>
-                        <TableCell className='px-4 py-4'>
-                           {itemMenu.minimumstock} / {itemMenu.maximumstock}
-                        </TableCell>
-                        <TableCell className='px-4 py-4'>
-                           R${' '}
-                           {itemMenu.purchaseprice.toLocaleString('pt-BR', {
-                              minimumFractionDigits: 2,
-                              maximumFractionDigits: 2,
-                           })}
-                        </TableCell>
-                        <TableCell className='px-4 py-4'>
-                           R${' '}
-                           {itemMenu.sellingprice.toLocaleString('pt-BR', {
-                              minimumFractionDigits: 2,
-                              maximumFractionDigits: 2,
-                           })}
-                        </TableCell>
-                        <TableCell className='px-4 py-4'>
-                           <p className='max-w-96 overflow-hidden line-clamp-2 text-ellipsis whitespace-normal text-gray-500'>
-                              {itemMenu.itemdescription}
-                           </p>
-                        </TableCell>
-                        <TableCell className='px-4 py-4'>{itemMenu.supplier}</TableCell>
-                        <TableCell className='px-4 py-4'>
-                           <Badge
-                              className={cn(
-                                 itemMenu.status === 'Normal' ? 'bg-emerald-500 text-white' : 'bg-red-500 text-white',
-                                 'rounded-full'
-                              )}
+         <div className='border border-gray-200 rounded-md w-full'>
+            {itemsMenu?.length === 0 ? (
+               <p className='text-gray-500 p-6 text-center'>Você não possui item no estoque.</p>
+            ) : (
+               <Table>
+                  <TableHeader>
+                     <TableRow>
+                        {itemsHead.map((itemHead) => (
+                           <TableHead
+                              key={itemHead}
+                              className='text-gray-500 pl-4'
                            >
-                              {itemMenu.status}
-                           </Badge>
-                        </TableCell>
-                        <TableCell className='py-4'>
-                           <Button
-                              className='cursor-pointer'
-                              variant='ghost'
-                              size='sm'
-                           >
-                              <Edit size={16} />
-                           </Button>
-                           <Button
-                              className='cursor-pointer'
-                              variant='ghost'
-                              size='sm'
-                           >
-                              <Trash2 size={16} />
-                           </Button>
-                        </TableCell>
+                              {itemHead}
+                           </TableHead>
+                        ))}
                      </TableRow>
-                  ))}
-               </TableBody>
-            </Table>
+                  </TableHeader>
+
+                  <TableBody>
+                     {itemsMenu?.map((itemMenu) => (
+                        <TableRow
+                           key={itemMenu.id}
+                           className='hover:bg-muted/50 border-b border-gray-200'
+                        >
+                           <TableCell className='px-4 py-4'>{itemMenu.itemname}</TableCell>
+                           <TableCell className='px-4 py-4'>{itemMenu.itemquantity}</TableCell>
+                           <TableCell className='px-4 py-4'>
+                              {itemMenu.minimumstock} / {itemMenu.maximumstock}
+                           </TableCell>
+                           <TableCell className='px-4 py-4'>
+                              R${' '}
+                              {itemMenu.purchaseprice.toLocaleString('pt-BR', {
+                                 minimumFractionDigits: 2,
+                                 maximumFractionDigits: 2,
+                              })}
+                           </TableCell>
+                           <TableCell className='px-4 py-4'>
+                              R${' '}
+                              {itemMenu.sellingprice.toLocaleString('pt-BR', {
+                                 minimumFractionDigits: 2,
+                                 maximumFractionDigits: 2,
+                              })}
+                           </TableCell>
+                           <TableCell className='px-4 py-4'>
+                              <p className='max-w-96 overflow-hidden line-clamp-2 text-ellipsis whitespace-normal text-gray-500'>
+                                 {itemMenu.itemdescription}
+                              </p>
+                           </TableCell>
+                           <TableCell className='px-4 py-4'>{itemMenu.supplier}</TableCell>
+                           <TableCell className='px-4 py-4'>
+                              <Badge
+                                 className={cn(
+                                    itemMenu.status === 'Normal'
+                                       ? 'bg-emerald-500 text-white'
+                                       : 'bg-red-500 text-white',
+                                    'rounded-full'
+                                 )}
+                              >
+                                 {itemMenu.status}
+                              </Badge>
+                           </TableCell>
+                           <TableCell className='py-4'>
+                              <Button
+                                 className='cursor-pointer'
+                                 variant='ghost'
+                                 size='sm'
+                              >
+                                 <Edit size={16} />
+                              </Button>
+                              <Button
+                                 className='cursor-pointer'
+                                 variant='ghost'
+                                 size='sm'
+                              >
+                                 <Trash2 size={16} />
+                              </Button>
+                           </TableCell>
+                        </TableRow>
+                     ))}
+                  </TableBody>
+               </Table>
+            )}
          </div>
       </div>
    );
