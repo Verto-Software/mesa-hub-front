1c936ada45fc0608856dd30f4e1782a0cefc125e
Jack <ferreira.marlon@live.com>
2025-07-20 19:24:45 -0300

    feat: adiciona a lista em ordem crescente pelo número da comanda

--- src/modules/order/order-card/order-card-item.tsx
+++ src/modules/order/order-card/order-card-item.tsx
@@ -15,41 +15,43 @@ export function OrderCardItem() {
 
    return (
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 px-6'>
-         {orders.map((order) => {
-            return (
-               <Card
-                  key={order.id}
-                  className='cursor-pointer select-none active:scale-[0.99] hover:-translate-y-0.8 hover:bg-gray-50 hover:shadow-lg transition-all duration-300 mt-0.5'
-                  role='button'
-                  tabIndex={0}
-                  onClick={() => handleCardDetail(order.id)}
-               >
-                  <CardContent>
-                     <div className='flex items-center w-full justify-between flex-wrap'>
-                        <p className='text-md font-medium flex items-center gap-2'>
-                           <Hash
-                              size={14}
-                              className='text-gray-500 hover: group-hover:text-white'
-                           />
-                           {order.number}
-                        </p>
-                        <p className='text-md font-medium flex items-center gap-1'>
-                           <User
-                              size={18}
-                              className='text-gray-500 hover: group-hover:text-white'
-                           />
-                           {order.name}
-                        </p>
-                     </div>
-                     {order.description && (
-                        <CardDescription className='mt-3 group-hover:text-white'>
-                           <p>{order.description}</p>
-                        </CardDescription>
-                     )}
-                  </CardContent>
-               </Card>
-            );
-         })}
+         {[...orders]
+            .sort((a, b) => Number(a.number) - Number(b.number))
+            .map((order) => {
+               return (
+                  <Card
+                     key={order.id}
+                     className='cursor-pointer select-none active:scale-[0.99] hover:-translate-y-0.8 hover:bg-gray-50 hover:shadow-lg transition-all duration-300 mt-0.5'
+                     role='button'
+                     tabIndex={0}
+                     onClick={() => handleCardDetail(order.id)}
+                  >
+                     <CardContent>
+                        <div className='flex items-center w-full justify-between flex-wrap'>
+                           <p className='text-md font-medium flex items-center gap-2'>
+                              <Hash
+                                 size={14}
+                                 className='text-gray-500 hover: group-hover:text-white'
+                              />
+                              {order.number}
+                           </p>
+                           <p className='text-md font-medium flex items-center gap-1'>
+                              <User
+                                 size={18}
+                                 className='text-gray-500 hover: group-hover:text-white'
+                              />
+                              {order.name}
+                           </p>
+                        </div>
+                        {order.description && (
+                           <CardDescription className='mt-3 group-hover:text-white'>
+                              <p>{order.description}</p>
+                           </CardDescription>
+                        )}
+                     </CardContent>
+                  </Card>
+               );
+            })}
       </div>
    );
 }
