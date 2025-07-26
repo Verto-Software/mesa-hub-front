0586e2c0b5b2161e878432df8766acbb52b18d43
Jack <ferreira.marlon@live.com>
2025-07-26 01:01:34 -0300

    style: diminui o tamanho da letra do titulo do header

--- src/_shared/components/ui/header.tsx
+++ src/_shared/components/ui/header.tsx
@@ -6,11 +6,11 @@ import { SidebarTrigger } from './sidebar';
 
 export function Header() {
    return (
-      <header className='flex items-center gap-4 border-b p-5 select-none sticky top-0 bg-white z-10 print:hidden'>
+      <header className='flex items-center gap-4 border-b p-5.5 select-none sticky top-0 bg-white z-10 print:hidden'>
          <SidebarTrigger className='cursor-pointer' />
          <Separator orientation='vertical' />
-         <h1 className='text-2xl font-semibold mb-1'>Sistema de Gestão</h1>
-         <ButtonFullScreen className='cursor-pointer w-8 h-8 ml-auto' />
+         <h1 className='text-xl font-semibold'>Sistema de Gestão</h1>
+         <ButtonFullScreen className='cursor-pointer w-8 h-8 ml-auto hidden lg:flex' />
       </header>
    );
 }
